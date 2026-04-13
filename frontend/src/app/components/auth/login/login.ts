87e2f1935091ec159e9556ee0d4d610a5662cbc6
Edder H F <edhernandezfo@unal.edu.co>
2026-04-12 22:27:21 -0500

    feat: login

--- frontend/src/app/components/auth/login/login.ts
+++ frontend/src/app/components/auth/login/login.ts
@@ -2,21 +2,26 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
 import { Footer } from '../../footer/footer';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [FormsModule, Footer],
   templateUrl: './login.html',
   styleUrl: './login.css',
-  providers: [AuthService]
 })
 export class Login {
   public request: any;
-  constructor(private authService: AuthService) {
+  public errorMessage: string;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
     this.request = {
       email: '',
       password: '',
     }
+    this.errorMessage = '';
 
 
   }
@@ -24,10 +29,10 @@ export class Login {
   onSubmit(): void {
     this.authService.login(this.request).subscribe({
       next: (response) => {
-        // Handle successful login
+        this.router.navigate(['/home']);
       },
       error: (error) => {
-        // Handle login error
+        this.errorMessage = 'Credenciales inválidas. Por favor, inténtalo de nuevo.';
       }
     });
   }
