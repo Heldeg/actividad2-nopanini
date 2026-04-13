2aefa11a051260481ad1dd8df5846f9494f7a0e4
Edder H F <edhernandezfo@unal.edu.co>
2026-04-12 23:03:58 -0500

    feat: add login to navbar

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { Categories } from '../categories/categories';
+import { AuthService } from '../../services/auth/auth.service';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [Categories],
+  imports: [Categories, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  public authService: AuthService = inject(AuthService);
+  public isLoggedIn: WritableSignal<boolean>;
+  constructor(private router: Router) {
+    this.isLoggedIn = signal<boolean>(this.authService.isLoggedIn());
+  }
   protected readonly categories = [
     'Todos',
     'Clasicos',
@@ -25,4 +33,21 @@ export class Header {
     { category: 'Misterio', title: 'El Codigo Da Vinci' },
     { category: 'Ciencia', title: 'Breve Historia del Tiempo' },
   ];
+
+  onLogin() {
+    this.router.navigate(['/login']);
+  }
+
+
+  onLogout() {
+    this.authService.logout().subscribe({
+      next: (info) => {
+        console.log('Logout successful:', info);
+        this.isLoggedIn.set(this.authService.isLoggedIn());
+      }, error: (error) => {
+        console.error('Logout error:', error);
+      }
+      
+    });
+  }
 }
--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 
 @Injectable({
@@ -29,6 +29,19 @@ export class AuthService {
     localStorage.setItem('authToken', token);
   }
 
+  private removeToken(): void {
+    localStorage.removeItem('authToken');
+  }
+
+  public getRole(): string | null {
+    return localStorage.getItem('userRole');
+  }
+
+  private removeRole(): void {
+    localStorage.removeItem('userRole');
+  }
+
+
   public getToken(): string | null {
     return localStorage.getItem('authToken');
   }
@@ -37,8 +50,19 @@ export class AuthService {
     return this.getToken() !== null;
   }
   
-  public logout(): void {
+  public logout(): Observable<any> {
+    const token = this.getToken();
+    
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
 
+    return this.http.post(`${this.apiUrl}/logout`, {}, { headers }).pipe(
+      tap(() => {
+        this.removeToken();
+        this.removeRole();
+      })
+    );
   }
   public register() {
 
